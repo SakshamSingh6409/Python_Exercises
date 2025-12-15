e422c205835629001b90078726c5a1b4a989a48b
SakshamSingh2306 <sakshamsingh6409@gmail.com>
2025-12-15 23:56:01 +0530

    Factorial

--- Facorial.py
+++ Facorial.py
@@ -1,4 +1,7 @@
 #This is a program to find the factorial of a inputed number using a for loop and recursion methods.
+import math
+def factorial_math(n):
+    return math.factorial(n)
 
 def factorial_for_loop(n):
     result = 1
@@ -15,3 +18,4 @@ def factorial_recursive(n):
 n=int(input("Enter a number to find its factorial: "))
 print(f'Factorial using recursive method: {factorial_recursive(n)}')
 print(f'Factorial using for loop method: {factorial_for_loop(n)}')
+print(f'Factorial using math module: {factorial_math(n)}')
